a506b9d8d8460cd3a7edf625fdc973360127c923
Wilson <w.cerdaespinoza@uandresbello.edu>
2025-05-05 22:48:52 -0400

    Cambios para que funcione

--- Acortador-Url-React/src/App.jsx
+++ Acortador-Url-React/src/App.jsx
@@ -4,6 +4,9 @@ import UrlList from "./components/UrlList";
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <div>
+          <p>API URL: {import.meta.env.VITE_API_BASE_URL}</p>
+        </div>
         <h1 className="text-3xl font-bold text-blue-600 mb-4">
         Acortador Url
         </h1>
--- Acortador-Url-React/src/api/axiosInstance.js
+++ Acortador-Url-React/src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.log("🧪 BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/urls',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosInstance;
